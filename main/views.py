1c6a4872722989fa0413fe64da1f0ace7be2544d
Marin260 <marin.martuslovic@gmail.com>
2022-02-05 23:52:55 +0100

    events ordered by date posted | tests updated

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import *
 
 def homepage(request):
-    events = Event.objects.all()
+    events = Event.objects.all().order_by('-date_posted')
     context = {'events' : events}
     return render(request, "./base_generic.html", context)
 
