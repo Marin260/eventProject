7bd6dd6d2a63a20923a3d5a4461e3c98d3713ca5
Marin260 <marin.martuslovic@gmail.com>
2022-02-06 19:17:48 +0100

    added static folder

--- eventProject/settings.py
+++ eventProject/settings.py
@@ -136,6 +136,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
