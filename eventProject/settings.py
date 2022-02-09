44966eb62ef4145160da1ccbd9e31004555c18c6
Marin260 <marin.martuslovic@gmail.com>
2022-02-10 00:10:47 +0100

    heroku changes

--- eventProject/settings.py
+++ eventProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -157,3 +158,4 @@ ACCOUNT_LOGOUT_ON_GET = True
 ACCOUNT_LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_PRESERVE_USERNAME_CASING = False
 
+django_heroku.settings(locals())
